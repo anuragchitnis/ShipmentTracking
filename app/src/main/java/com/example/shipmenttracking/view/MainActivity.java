e07016198ef69c8f3a78011515e633a425bd8102
Anurag Chitnis <chitnis.anu@gmail.com>
2017-12-05 14:53:52 -0700

    Improved the layout, implemented data binding

--- app/src/main/java/com/example/shipmenttracking/view/MainActivity.java
+++ app/src/main/java/com/example/shipmenttracking/view/MainActivity.java
@@ -1,49 +1,31 @@
 package com.example.shipmenttracking.view;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
 
 import com.example.shipmenttracking.R;
+import com.example.shipmenttracking.databinding.ActivityMainBinding;
 import com.example.shipmenttracking.viewmodel.ShipmentViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private ShipmentViewModel viewModel;
-    private TextView trackingNumberText;
-    private TextView timestampText;
-    private TextView shipmentCreateDateText;
-    private EditText trackingIdEditText;
-    private Button submitButton;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         viewModel = ViewModelProviders.of(this).get(ShipmentViewModel.class);
+    }
 
-        trackingNumberText = (TextView) findViewById(R.id.trackingNumber);
-        timestampText = (TextView) findViewById(R.id.timestamp);
-        shipmentCreateDateText = (TextView) findViewById(R.id.shipmentCreateDate);
-        trackingIdEditText = (EditText) findViewById(R.id.trackingIdEditText);
-        submitButton = (Button) findViewById(R.id.submitButton);
-
-        trackingIdEditText.setText("220088641150");
-
-        submitButton.setOnClickListener((view) -> {
-            //"220088641150"
-            viewModel.getShipment(trackingIdEditText.getText().toString()).observe(this, shipment ->
-            {
-                trackingNumberText.setText(shipment.getTrackingNumber());
-                timestampText.setText(String.valueOf(shipment.getTimestamp()));
-                shipmentCreateDateText.setText(String.valueOf(shipment.getShipmentCreateDate()));
-            });
+    public void onSubmitClicked(View view) {
+        viewModel.getShipment(binding.trackingIdEditText.getText().toString()).observe(this, shipment ->
+        {
+            binding.setShipment(shipment);
         });
-
-
     }
 }
